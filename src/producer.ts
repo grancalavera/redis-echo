6521b32fe56874facc068cc567b8c5d5ca1f5eb3
Leon Coto <leoncoto@gmail.com>
2020-07-22 22:00:56 +0100

    simple streams example

--- src/producer.ts
+++ src/producer.ts
@@ -1,24 +1,2 @@
-import Redis from "ioredis";
-const r = new Redis();
+import r from "ioredis";
 import { groupName, streamName } from "./consumer-group";
-
-main();
-
-async function main() {
-  try {
-    await r.xgroup("create", streamName, groupName, "$", "mkstream");
-  } catch (e) {
-    console.log(e);
-  }
-
-  const messages = ["apple", "orange", "strawberry", "apricot", "banana"];
-
-  for (const message of messages) {
-    try {
-      const result = await r.xadd(streamName, "*", "message", message);
-      console.log(`${message}: ${result}`);
-    } catch (e) {
-      console.error(`failed to add message ${message}`);
-    }
-  }
-}
